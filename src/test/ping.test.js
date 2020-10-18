160142c87a70fa66e58ddfeec46dfe0a439f35c6
Alex Samari <alexsamarim@gmail.com>
2020-10-17 23:29:16 -0400

    create a test for ping API

---
+++ src/test/ping.test.js
@@ -0,0 +1,35 @@
+'use strict';
+
+import Lab from '@hapi/lab';
+import expect from '@hapi/code';
+
+const { afterEach, beforeEach, describe, it } = exports.lab = Lab.script();
+const { init } = require('../lib/server');
+
+describe('GET /api/ping', () => {
+	let server;
+
+	beforeEach(async () => {
+		server = await init();
+	});
+
+	afterEach(async () => {
+		await server.stop();
+	});
+
+	it('responds with 200', async () => {
+		const res = await server.inject({
+			method: 'get',
+			url: '/api/ping'
+		});
+		expect(res.statusCode).to.equal(200);
+	});
+
+	it('has correct response body', async () => {
+		const res = await server.inject({
+			method: 'get',
+			url: '/api/ping'
+		});
+		expect(body).to.equal('{"success":"true"}');
+	});
+});
